8f35dd7401c2e39472adbc4b055c4715bc531ae6
Beo kutee <hunglcvph28989@fpt.edu.vn>
2024-03-27 00:37:33 +0700

    crud cmt

--- src/controllers/comments.js
+++ src/controllers/comments.js
@@ -3,9 +3,24 @@ import Comment from "../models/comment";
 export const getAllCommentsByLesson = async (req, res) => {
   try {
     const { id } = req.params;
-    const comments = await Comment.find({ lesson_id: id });
+    const comments = await Comment.find();
     res.status(200).send({
       message: "Lấy thành công toàn bộ bình luận",
+      data: comments,
+    });
+  } catch (e) {
+    res.status(500).send({
+      message: e.message,
+    });
+  }
+};
+export const getCommentById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const comments = await Comment.findById(id);
+    console.log(comments)
+    res.status(200).send({
+      message: "Lấy thành công bình luận",
       data: comments,
     });
   } catch (e) {
@@ -35,3 +50,57 @@ export const postComment = async (req, res) => {
     });
   }
 };
+export const deleteComment = async (req, res) => {
+  try {
+    const findCmt = await Comment.findById(req.params.id)
+    if (findCmt){
+      const result = await Comment.deleteOne({ _id: req.params.id });
+      return res.status(200).json({
+        error: 0,
+        data: findCmt,
+        result: result,
+        message: "Xóa thành công",
+      });
+    }
+    else {
+      res.status(404).json({
+        error: 1,
+        message: "Không tìm thấy bình luận",
+      });
+    }
+  } catch (error) {
+    console.log("Error: delete comment", error);
+    res.status(500).json({
+      error: 1,
+      message: error,
+    });
+  }
+}
+export const editComment = async (req, res) => {
+  try {
+    const findCmt = await Comment.findById(req.params.id)
+    const data = req.body
+    if (findCmt){
+      const result = await User.patch({ _id: req.params.id },data);
+      return res.status(200).json({
+        error: 0,
+        data: findCmt,
+        result: result,
+        message: "Sửa thành công",
+      });
+    }
+    else {
+      res.status(404).json({
+        error: 1,
+        message: "Không tìm thấy bình luận",
+      });
+    }
+  } catch (error) {
+    console.log("Error: update comment", error);
+    res.status(500).json({
+      error: 1,
+      message: error,
+    });
+  }
+}
+
--- src/routers/comments.js
+++ src/routers/comments.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getAllCommentsByLesson, postComment } from "../controllers/comments";
+import { deleteComment, editComment, getAllCommentsByLesson, getCommentById, postComment } from "../controllers/comments";
 
 const commentRoute = new Router();
 
-commentRoute.get("/:id", getAllCommentsByLesson);
+commentRoute.get("/", getAllCommentsByLesson);
+commentRoute.get("/:id", getCommentById);
 commentRoute.post("/", postComment);
-
+commentRoute.delete("/:id", deleteComment)
+commentRoute.patch("/:id",editComment)
 export default commentRoute;
