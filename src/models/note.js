29744fa13146b8218691c1e3c8bb4a18248c76f5
unknown <longmobietv@gmail.com>
2024-05-15 22:06:07 +0700

    fix: update course

--- src/models/note.js
+++ src/models/note.js
@@ -1,23 +1,23 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema } from 'mongoose';
 
 const Notes = Schema(
-  {
-    text: {
-      type: String,
-      require: true,
+    {
+        text: {
+            type: String,
+            require: true,
+        },
+        user_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'users',
+            require: true,
+        },
+        lesson_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'lessons',
+            require: true,
+        },
     },
-    user_id: {
-      type: Schema.Types.ObjectId,
-      ref: "users",
-      require: true,
-    },
-    lesson_id: {
-      type: Schema.Types.ObjectId,
-      ref: "lessons",
-      require: true,
-    },
-  },
-  { timestamps: true, versionKey: false }
+    { timestamps: true, versionKey: false },
 );
 
-export default mongoose.model("notes", Notes);
+export default mongoose.model('notes', Notes);
